142e6ee1bc92270d9d67d9fe0ac5d2071e0ff70e
Arnaud Vrac <avrac@freebox.fr>
2018-02-23 09:55:46 +0100

    add debug macro

--- qdl.h
+++ qdl.h
@@ -10,6 +10,11 @@ int firehose_run(int fd);
 int sahara_run(int fd, char *prog_mbn);
 void print_hex_dump(const char *prefix, const void *buf, size_t len);
 
+#define dbg(fmt, ...) do {			\
+	if (qdl_debug)				\
+		printf(fmt, ##__VA_ARGS__);	\
+} while (0)
+
 extern bool qdl_debug;
 
 #endif
